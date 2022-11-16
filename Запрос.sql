6421776918e911976e3691975e56d36571d48c2a
Sergey Kobzev <117295362+hediogenes@users.noreply.github.com>
2022-11-16 15:12:26 +0300

    Update Запрос.sql

--- Запрос.sql
+++ Запрос.sql
@@ -2,6 +2,12 @@ SELECT customer_unique_id AS customer,
        DATE(order_purchase_timestamp) AS purchase_date,
        price AS income_per_customer,
        SUM(price) OVER w AS full_income_per_customer
+/* В SELECT я вложил следующие колонки:
+* customer_unique_id содержит уникальные ID всех пользователей. Я присвоил ей название customer.
+* order_purchase_timestamp содержит дату и время покупки. Я оставил от нее только дату (с помощью DATE()) и присвоил ей название purchase_date.
+* price содержит цену каждого заказа. Я присвоил ей название income_per_customer.
+* Я вновь вложил price, но уже для реализации оконной функции, которая будет считать сумму принесенной прибыли. Я присвоил ей название full_income_per_customer.
+*/
 FROM e_shop.olist_order_items_dataset a LEFT JOIN e_shop.olist_orders_dataset b ON (a.order_id = b.order_id)
                                         LEFT JOIN e_shop.olist_customers_dataset c ON (b.customer_id = c.customer_id)
 WHERE customer_unique_id IN 
